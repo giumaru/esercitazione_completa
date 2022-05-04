c6bebefe746ef23a770e57c28d213825d0cd7b99
giumaru <104774224+giumaru@users.noreply.github.com>
2022-05-04 14:56:24 +0200

    Add files via upload

--- p_1.cpp
+++ p_1.cpp
@@ -1,10 +1,48 @@
 #include <iostream>
+#define DIM 50
 
 using namespace std;
 
+struct Anag
+{
+    string isbn, titolo, autore, editore, anno, prezzo, tag;
+};
+
+int inserimento(Anag libro[])
+{
+    return 0;
+}
+
+void stampa (Anag libro[], int n)
+{
+
+}
+
+int ric(Anag libro[], int n)
+{
+    return 0;
+}
+
+void modifica_campi(Anag libro[], int n)
+{
+
+}
+
+void elimina_record(Anag libro[], int n)
+{
+    
+}
+
+void menu(int n, Anag libro[], int m)
+{
+    
+}
+
 int main()
 {
-    cout<<"Hello World";
-
+    int n, m;
+    Anag libro[DIM];
+    menu(n, libro, m);
+    
     return 0;
 }
