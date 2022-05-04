efa337a8deaca2113e588f80fbed81f087873865
giumaru <104774224+giumaru@users.noreply.github.com>
2022-05-04 15:07:34 +0200

    Add files via upload

--- p_1.cpp
+++ p_1.cpp
@@ -35,7 +35,37 @@ void elimina_record(Anag libro[], int n)
 
 void menu(int n, Anag libro[], int m)
 {
-    
+    do{
+    cout<<"1) Inserire libro "<<endl<<"2) Visualizzare l’intero catalogo della biblioteca "<<endl<<"3) Cancellare un libro "<<endl<<"4) Ricercare un particolare libro "<<endl<<"5) Effettuare delle modifiche"<<endl<<"6) Uscita"<<endl;
+    cin>>m;
+
+    if(m==1)
+        n=inserimento(libro);
+        cout<<endl;
+
+    if(m==2)
+        stampa (libro, n);
+        cout<<endl;
+
+    if(m==3)
+        elimina_record(libro, n);
+        cout<<endl;
+
+    if(m==4)
+        int i=ric(libro, n);
+        cout<<endl;
+
+    if(m==5)
+        int i=ric(libro, n);
+        cout<<endl;
+        
+        modifica_campi(libro, n);
+        cout<<endl;
+
+    if(m==6)
+        cout<<"Uscito.";
+
+    }while(m!=6);
 }
 
 int main()
